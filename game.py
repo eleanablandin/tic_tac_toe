cbe4e0910b6c89bc5fef72bf0d92d8cb75ec45ea
Eleana Peña <eleanablandin@gmail.com>
2024-05-21 20:53:21 -0400

    adding user_inpit and game function. The code is working

--- game.py
+++ game.py
@@ -20,9 +20,9 @@ def check_winner(board, player):
     
     # check diagonals
     if board[0][0] == board[1][1] == board[2][2] == player:
-        True
+        return True
     if board[0][2] == board[1][1] == board[2][0] == player:
-        True
+        return True
 
     return False
 
@@ -81,8 +81,67 @@ def minimax(board, depth, is_maximizing):
         
         return best_score
 
+
+def best_move(board):
+        best_score = -math.inf
+        move = None
+
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == ' ':
+                    board[i][j] = 'O'
+                    score = minimax(board, 0, False)
+                    board[i][j] = ' '
+                    if score > best_score:
+                        best_score = score
+                        move = (i, j)
+        return move 
+
+def user_input(board):
+    while True:
+        try:
+            row = int(input('Pick a row:'
+                            '[upper row: enter 0, middle row: enter 1, bottom row: enter 2]:'))
+            col = int(input('Pick a column:'
+                            '[left column: enter 0, middle column: enter 1, right column enter 2]:'))
+            
+            if row in range(3) and col in range(3) and board[row][col] == ' ':
+                
+                return row, col
+            else:
+                print('Please enter a number(0, 1 or 2)')
+        except ValueError:
+            print('Invalid input. Numbers only')
+
+
 def game():
     board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+
+    while True:
+        create_board(board)
+
+        if current_player == 'X':
+            row, col = user_input(board)
+        else:
+            move = best_move(board)
+            if move:
+                row, col = move
+            else:
+                break
 
 
+        board[row][col] = current_player
 
+        if check_winner(board, current_player):
+             create_board(board)
+             print(f"Player {current_player} wins!")
+             break
+        elif check_draw(board):
+             create_board(board)
+             print('Its a draw!')
+             break 
+        
+        current_player = "O" if current_player == "X" else "X"
+
+game()
